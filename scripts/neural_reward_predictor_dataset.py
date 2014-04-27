3bf904261d683a2e0d79bd727c78070dbc5da91c
Brian Brown <brian.brown473@gmail.com>
2014-04-27 18:42:55 -0400

    added reward predictor

---
+++ scripts/neural_reward_predictor_dataset.py
@@ -0,0 +1,257 @@
+from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+from pylearn2.datasets.dense_design_matrix import DefaultViewConverter
+from theano import ProfileMode
+import numpy as np
+import theano
+from theano.compat.python2x import OrderedDict
+from pylearn2.space import VectorSpace
+from pylearn2.utils import safe_zip
+import theano.tensor as T
+import copy
+import os
+from itertools import izip
+from pylearn2.utils.data_specs import DataSpecsMapping
+import Image
+import matplotlib.pyplot as plt
+import time
+
+from random import Random
+
+class NeuralRewardPredictorDataset:
+    randGenerator=Random()
+
+
+    def __init__(self, cnn, mini_batch_size, num_mini_batches, history_size = 1000000, \
+                    gamma = .9, learning_rate = .5):
+        """
+        Sets up variables for later use.
+        
+        """
+        self.print_cost = False
+        self.mini_batch_size = mini_batch_size
+        self.num_mini_batches = num_mini_batches
+        self.learning_rate = learning_rate
+        self.history_size = history_size
+        self.gamma = gamma
+        self.data = []
+        self.actions = []
+        self.rewards = []
+        self.terminal = []
+        self.counter = 0
+        self.action = 0
+        self.cnn = cnn
+        self.image_shape = (4, 80, 80, self.mini_batch_size * self.num_mini_batches)
+        
+        self.rms_vals = []
+        
+        self.rms_vals.append(theano.shared(np.ones((4, 8, 8, 16), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((16, 19, 19), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((16, 4, 4, 32), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((32, 9, 9), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((2592, 256), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((256,), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((256, 3), dtype='float32')))
+        self.rms_vals.append(theano.shared(np.ones((3,), dtype='float32')))
+        
+        self.setup_training()
+        
+    def setup_training(self):
+        """
+        Sets up training function.
+        """
+        
+        training_batch_size = self.mini_batch_size
+        
+        cost = self.cnn.get_default_cost()
+        
+        data_specs = cost.get_data_specs(self.cnn)
+        mapping = DataSpecsMapping(data_specs)
+        space_tuple = mapping.flatten(data_specs[0], return_tuple=True)
+        source_tuple = mapping.flatten(data_specs[1], return_tuple=True)
+        
+        theano_args = []
+        for space, source in safe_zip(space_tuple, source_tuple):
+            name = '%s[%s]' % (self.__class__.__name__, source)
+            arg = space.make_theano_batch(name=name,
+                        batch_size=training_batch_size).astype("float32")
+            theano_args.append(arg)
+        theano_args = tuple(theano_args)
+        
+        y_hat = self.cnn.fprop(theano_args[0])
+        
+        self.fprop_func = theano.function([theano_args[0]], y_hat)
+        
+        cost = self.cnn.cost(theano_args[1], y_hat)
+        
+        lr_scalers = self.cnn.get_lr_scalers()
+        
+        params = list(self.cnn.get_params())
+        grads = T.grad(cost, params, disconnected_inputs='ignore')
+        
+        gradients = OrderedDict(izip(params, grads))
+        
+        rms_vals_dict = OrderedDict(izip(params, self.rms_vals))
+        
+        updates = OrderedDict()
+        
+        updates.update(dict(safe_zip(params, [param - self.learning_rate * 
+                                (gradients[param] / 
+                                T.sqrt(rms_vals_dict[param] + 1e-8)) 
+                                for param in params])))
+                                                    
+        rmsprop_updates = OrderedDict()
+        
+        rmsprop_updates.update(dict(safe_zip(self.rms_vals, [(rms_vals_dict[param] * .9) + 
+                                            (T.sqr(gradients[param]) * .1)
+                                                for param in params])))
+        
+        self.training = theano.function(theano_args, updates=updates, 
+                                        on_unused_input='ignore')
+                                        
+        self.rmsprop_update = theano.function(theano_args, updates=rmsprop_updates,
+                                                on_unused_input='ignore')
+        
+        temp = T.tensor4()
+        
+        self.dimshuf_func = theano.function([temp], temp.dimshuffle(1, 2, 3, 0))
+        
+        #self.grads_func = theano.function(theano_args, grads)
+                                        
+        self.cost_function = theano.function(theano_args, cost)
+        
+
+    def add(self, data, action, reward, terminal):
+        """
+        Used for adding data to the dataset.
+
+        """
+        image = Image.fromarray(data.reshape((80, 105)))
+        image = image.resize((80, 80))
+        
+        image = np.asarray(image)
+        
+        self.data.append(image.astype('uint8'))
+        self.actions.append(action)
+        self.rewards.append(reward)
+        self.terminal.append(terminal)
+
+        if len(self.data) > self.history_size:
+            self.data.pop(0)
+            self.actions.pop(0)
+            self.rewards.pop(0)
+            self.terminal.pop(0)
+
+    def has_targets(self):
+        return True
+
+    def __len__(self):
+        return len(self.data)
+
+    def train(self):
+        formatted_data = []
+        states = None
+        next_states = None
+        actions = []
+        rewards = []
+        terminals = []
+        
+        #create training batch
+        for i in range(self.mini_batch_size * self.num_mini_batches):
+            #select a random point in history, multiple of 4 so state matches
+            #action set
+            while True:
+                data_num = self.randGenerator.randint(8, len(self.data) - 5)
+                if data_num % 4 == 0:
+                    break
+                    
+            terminal_state = False
+            
+            #if the state is terminal, subtract 4
+            for i in range(4):
+                if self.terminal[data_num - i]:
+                    terminal_state = True
+                    data_num -= 4
+                    
+            #determine if the next state is terminal
+            for i in range(4):
+                if self.terminal[data_num + 4 - i]:
+                    terminal_state = True
+            
+            #put values into lists
+            if states == None:
+                states = self.get_state(data_num)
+                next_states = self.get_state(data_num + 4)
+            else:
+                states = np.append(states, self.get_state(data_num))
+                next_states = np.append(next_states, self.get_state(data_num + 4))
+            actions.append(self.actions[data_num + 1])
+            terminals.append(terminal_state)
+            
+            reward = 0
+            for i in range(4):
+                if self.rewards[i + 1 + data_num] != 0:
+                    reward += self.rewards[i + 1 + data_num]
+            
+            rewards.append(reward)
+        
+        #normalize values
+        states = states.reshape((32, 4, 80, 80)) / 256.0
+        next_states = next_states.reshape((32, 4, 80, 80)) / 256.0
+        
+        states = self.dimshuf_func(states)
+        next_states = self.dimshuf_func(next_states)
+        
+        #get output predictions from nn using the states batch
+        q_sa_list = self.fprop_func(states)
+        
+        #get max of Q(s, a)' for next state batch
+        q_sa_prime_list = self.fprop_func(next_states)
+        
+        print np.mean(q_sa_list)
+        #print ""
+        
+        #for param in self.cnn.get_params():
+        #    value = param.get_value(borrow=True)
+        #    if np.any(np.isnan(value)) or np.any(np.isinf(value)):
+        #        raise Exception("NaN in " + param.name)
+        
+        
+        for i in range(self.mini_batch_size * self.num_mini_batches):
+            q_sa_list[i][actions[i]] = rewards[i]
+        
+        if self.print_cost:
+            print self.cost_function(states, q_sa_list)
+
+        self.training(states, q_sa_list)
+        self.rmsprop_update(states, q_sa_list)
+        
+        if self.print_cost:
+            print self.cost_function(states, q_sa_list)
+        
+        
+        #for i in self.grads_func(states.reshape(self.image_shape), q_sa_list):
+        #    print i.shape
+        
+
+    def get_state(self, num):
+        #combine four states at num
+            
+        state = np.append(self.data[num - 3].reshape((6400)).astype('float32'), self.data[num - 2].reshape((6400)).astype('float32'), axis=0)
+        state = np.append(state, self.data[num - 1].reshape((6400)).astype('float32'), axis=0)
+        state = np.append(state, self.data[num].reshape((6400)).astype('float32'), axis=0)
+        
+        return state
+
+    def get_cur_state(self):
+        #combine last four states
+        state = self.get_state(len(self.data) - 1)
+        
+        #divide by 256 to make values < 1 for neural net
+        state /= 256.0
+        
+        #create theano variables
+        state = theano.shared(state, name='input')
+        state = T.reshape(state, (4, 80, 80, 1))
+        state = T.cast(state, dtype='floatX')
+        return state
+        
---
+++ scripts/rl_glue_neural_reward_predictor.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python
+"""
+This agent interacts with an RL-Glue environment to collect
+a set of (s, a, r, absorb) experiences for batch reinforceement learning. 
+
+This uses the skeleton_agent.py file from the Python-codec of rl-glue
+as a starting point. 
+
+Author: Nathan Sprague
+"""
+
+
+# 
+# Copyright (C) 2008, Brian Tanner
+# 
+#http://rl-glue-ext.googlecode.com/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import copy
+import cPickle
+import theano
+from rlglue.agent.Agent import Agent
+from rlglue.agent import AgentLoader as AgentLoader
+from rlglue.types import Action
+from rlglue.types import Observation
+from rlglue.utils import TaskSpecVRLGLUE3
+from pylearn2.training_algorithms import sgd
+from pylearn2.models import mlp
+from pylearn2.models import maxout
+from pylearn2.space import Conv2DSpace
+from pylearn2.termination_criteria import EpochCounter
+import neural_reward_predictor_dataset as nqd
+import time
+import theano.tensor as T
+import temp_mlp
+
+from random import Random
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import time
+
+class NeuralQLearnAgent(Agent):
+    randGenerator=Random()
+
+    def agent_init(self,task_spec_string):
+        """ 
+        This function is called once at the beginning of an experiment.
+
+        Arguments: task_spec_string - A string defining the task.  This string
+                                      is decoded using 
+                                      TaskSpecVRLGLUE3.TaskSpecParser
+        """
+        self.start_time = time.time()
+        self.image = None
+        self.show_ale = False
+        self.total_reward = 0
+        self.mini_batch_size = 32
+        self.num_mini_batches = 1
+        self.frame_count = 0
+        self.frames_trained = 0
+        self.qvalue_sum = 0
+        self.qvalue_count = 0
+        self.predicted_reward
+        learning_rate = .00001
+        self.testing_policy = False
+        self.epoch_counter = 0
+        self.epochs_until_test = 5
+        self.policy_test_file_name = "results.csv"
+        load_file = False
+        load_file_name = "cnnparams.pkl"
+        self.save_file_name = "cnnparams.pkl"
+        self.counter = 0
+        self.cur_action = 0
+        
+        #starting value for epsilon-greedy
+        self.epsilon = 1
+
+        TaskSpec = TaskSpecVRLGLUE3.TaskSpecParser(task_spec_string)
+        if TaskSpec.valid:
+            
+            assert ((len(TaskSpec.getIntObservations())== 0) != \
+                (len(TaskSpec.getDoubleObservations()) == 0 )), \
+                "expecting continous or discrete observations.  Not both."
+            assert len(TaskSpec.getDoubleActions())==0, \
+                "expecting no continuous actions"
+            assert not TaskSpec.isSpecial(TaskSpec.getIntActions()[0][0]), \
+                " expecting min action to be a number not a special value"
+            assert not TaskSpec.isSpecial(TaskSpec.getIntActions()[0][1]), \
+                " expecting max action to be a number not a special value"
+            self.num_actions=TaskSpec.getIntActions()[0][1]+1
+
+        self.num_actions = 3
+        
+        self.int_states = len(TaskSpec.getIntObservations()) > 0
+
+        # Create neural network and initialize trainer and dataset
+        
+        if load_file:
+            thefile = open(load_file_name, "r")
+            
+            self.cnn = cPickle.load(thefile)
+        else:
+        
+            self.first_conv_layer = maxout.MaxoutConvC01B(16, 1, (8, 8), (1, 1), 
+                            (1, 1), "first conv layer", irange=.1, 
+                                            kernel_stride=(4, 4), min_zero=True)
+                                            
+            self.second_conv_layer = maxout.MaxoutConvC01B(32, 1, (4, 4), 
+                            (1, 1), (1, 1), "second conv layer", irange=.1, 
+                                            kernel_stride=(2, 2), min_zero=True)
+                                            
+            self.rect_layer = mlp.RectifiedLinear(dim=256, 
+                            layer_name="rectified layer", irange=.1)
+                            
+            self.output_layer = mlp.Linear(self.num_actions, "output layer", 
+                            irange=.1)
+
+            layers = [self.first_conv_layer, self.second_conv_layer, 
+                            self.rect_layer, self.output_layer]
+
+            self.cnn = mlp.MLP(layers, input_space = Conv2DSpace((80, 80), 
+                                    num_channels=4, axes=('c', 0, 1, 'b')), 
+                                    batch_size=self.mini_batch_size)
+
+        self.data = nqd.NeuralRewardPredictorDataset(self.cnn, mini_batch_size = self.mini_batch_size, 
+                                            num_mini_batches = self.num_mini_batches, 
+                                            learning_rate=learning_rate)
+
+        #Create appropriate RL-Glue objects for storing these.
+        self.last_action=Action()
+        self.last_observation=Observation()
+
+        thefile = open(self.policy_test_file_name, "w")
+        thefile.write("Reward, Predicted reward, Frames trained\n")
+        thefile.close()
+
+
+
+    def agent_start(self,observation):
+        """  
+        This method is called once at the beginning of each episode. 
+        No reward is provided, because reward is only available after
+        an action has been taken. 
+
+        Arguments: 
+           observation - An observation of type rlglue.types.Observation
+
+        Returns: 
+           An action of type rlglue.types.Action
+        """
+
+        self.start_time = time.time()
+        this_int_action = self.get_action()
+        return_action=Action()
+        return_action.intArray = [this_int_action]
+
+
+        self.last_action = copy.deepcopy(return_action)
+        self.last_observation = copy.deepcopy(observation)
+
+        return return_action
+        
+    def get_action(self):
+        """
+        Get predictions on the last state to get the next action from qvalues.
+        Uses epsilon-greedy.
+        """
+        self.counter += 1
+        if self.counter != 4:
+            action = self.cur_action
+        else:
+            action = self.get_action_valself.randGenerator.randint(0,self.num_actions-1))
+            
+       if len(self.data) > 7:
+            state = self.data.get_state(len(self.data) - 1).astype('float32').reshape((4, 80, 80, 1))
+            
+            state /= 256.0
+            
+            qvalues = self.data.fprop_func(state)
+            
+            qvalues = qvalues.tolist()
+            
+            reward_prediction = qvalues[0][self.get_val_action(action)]
+            
+            if reward_prediction >= .5:
+                self.predicted_reward += 1
+        
+        if self.counter == 4:
+            self.counter = 0
+            self.cur_action = action
+
+        return action
+        
+    def get_action_val(self, val):
+        if val == 0:
+            return 1
+        elif val == 1:
+            return 7
+        elif val == 2:
+            return 8
+            
+    def get_val_action(self, val):
+        if val == 1:
+            return 0
+        elif val == 7:
+            return 1
+        elif val == 8:
+            return 2
+
+    def _show_ale_color(self):
+        """
+        Show an arcade learning environment screen.
+        """
+        plt.ion()
+        img = np.array(\
+            self.last_observation.intArray).reshape((160, 210, 3))
+        img = np.transpose(img, (1,0,2))
+        if self.image == None:
+            self.image = plt.imshow(img/256.0, interpolation='none')
+            fig = plt.gcf()
+            fig.set_size_inches(4,3)
+            plt.show()
+        else:
+            self.image.set_data(img/256.0) 
+        plt.draw()
+        time.sleep(.0)
+
+    def _show_ale_gray(self):
+        """
+        Show an arcade learning environment screen.
+        """
+        plt.ion()
+        img = np.array(\
+            self.last_observation.intArray).reshape((80, 105))
+        img = np.transpose(img)
+        if self.image == None:
+            self.image = plt.imshow(img/256.0, interpolation='none', \
+                                            cmap="gray")
+            fig = plt.gcf()
+            fig.set_size_inches(4,3)
+            plt.show()
+        else:
+            self.image.set_data(img/256.0)
+        plt.draw()
+        time.sleep(.0)
+
+    def agent_step(self, reward, observation):
+        """
+        This method is called each time step. 
+
+        Arguments: 
+           reward      - Real valued reward.
+           observation - An observation of type rlglue.types.Observation
+
+        Returns: 
+           An action of type rlglue.types.Action
+        
+        """
+        
+        #set reward to be either -1, 0, or 1 as described in atari paper
+        if reward > 0:
+            reward = 1
+		
+        if reward < 0:
+            reward = -1
+        
+        self.total_reward += reward
+        
+        self.frame_count += 1
+            
+        
+        self.data.add(self.last_observation.intArray, \
+                        self.get_val_action(self.last_action.intArray[0]), 
+                        reward, False)
+        
+        if len(self.data) > 1000 and not self.testing_policy:
+            self.frames_trained += 1
+            self.data.train()
+        
+        this_int_action = self.get_action()
+        return_action = Action()
+        return_action.intArray = [this_int_action]
+        
+        if self.show_ale:
+            #self._show_ale_color()
+            self._show_ale_gray()
+
+        self.last_action = copy.deepcopy(return_action)
+        self.last_observation = copy.deepcopy(observation)
+
+        return return_action
+
+    def agent_end(self, reward):
+        """
+        This function is called once at the end of an episode. 
+        
+        Arguments: 
+           reward      - Real valued reward.
+
+        Returns: 
+            None
+        """
+        if reward > 0:
+            reward = 1
+		
+        if reward < 0:
+            reward = -1
+        
+        self.total_reward += reward
+        
+        thefile = open(self.policy_test_file_name, "a")
+        
+        thefile.write(str(self.total_reward) +  ", ")
+        thefile.write(str(self.predicted_reward) + ", ")
+        thefile.write(str(self.frames_trained) + "\n")
+        thefile.close()
+        
+        self.total_reward = 0
+        self.predicted_reward = 0
+        self.qvalue_sum = 0
+        self.qvalue_count = 0
+        self.frame_count = 0
+        self.save_params(self.save_file_name)
+        
+        
+        self.data.add(self.last_observation.intArray, 
+                        self.get_val_action(self.last_action.intArray[0]), 
+                        reward, True)
+
+    def agent_cleanup(self):
+        """
+        Called once at the end of an experiment.  We could save results
+        here, but we use the agent_message mechanism instead so that
+        a file name can be provided by the experiment.
+        """
+        pass
+        
+    def save_params(self, filename="cnnparams.pkl"):
+        the_file = open(filename, "w")
+        
+        cPickle.dump(self.cnn, the_file, -1)
+
+    def agent_message(self, in_message):
+        """
+        The experiment will cause this method to be called.  Used
+        to save data to the indicated file. 
+        """
+        if in_message.startswith("save_data"):
+            total_time = time.time() - self.start_time
+            file_name=in_message.split(" ")[1]
+            the_file = open(file_name, "w")
+            all_data = (self.cnn.get_params())
+            print "PICKLING: " + file_name
+            #cPickle.dump(all_data, the_file, -1)
+            #print "Simulated at a rate of {}/s".format(len(self.rewards) / 
+            #                                           total_time)
+            return "File saved successfully"
+
+        else:
+            return "I don't know how to respond to your message"
+
+
+if __name__=="__main__":
+    AgentLoader.loadAgent(NeuralQLearnAgent())
